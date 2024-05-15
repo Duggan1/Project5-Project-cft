685957940c679133a475c4baa339d658db086e82
Kevin Duggan <kevinduggan95@gmail.com>
2024-05-15 11:54:36 -0500

    new address, functions

--- src/Home.js
+++ src/Home.js
@@ -22,16 +22,16 @@ function Home() {
         
             <h1>Chicago Fight Team</h1>
 
-            <p style={{ textAlign:'center' }}>
+            <a style={{ textAlign:'center' }}href="https://www.google.com/maps/place/8696+47th+St,+Lyons,+IL+60534/@41.8060748,-87.8398832,17z/data=!4m10!1m2!2m1!1schicago+fight+team++8696+w+47th+st+lyons+illinois+!3m6!1s0x880e361341753f5b:0x777f5a692510f0a6!8m2!3d41.8059629!4d-87.8363641!15sCjFjaGljYWdvIGZpZ2h0IHRlYW0gIDg2OTYgdyA0N3RoIHN0IGx5b25zIGlsbGlub2lzkgEQY29tcG91bmRfc2VjdGlvbuABAA!16s%2Fg%2F11bw4nfy1c?entry=ttu"
+     target="_blank" >
                 <span style={{ background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '5px', fontWeight: 'bold', textShadow: '0 0 15px black', }}>
-                6528 S Lavernge Ave Chicago, Illinois
-                </span>
-            </p>
-            <p style={{ textAlign:'center' }}>
+                8696 W 47th St Lyons, Illinois
+                </span> </a>
+                <a href="tel:+17082794734" style={{ textAlign:'center' }}>
                 <span style={{background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '5px', fontWeight: 'bold', textShadow: '0 0 15px black' }}>
                 1(773) 885-5700
                 </span>
-            </p>
+            </a>
 
             {/* <p>6528 S Lavernge Ave Chicago, Illinois</p>
             <p></p> */}
