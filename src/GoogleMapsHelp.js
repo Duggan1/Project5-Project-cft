dc90f266ac9d4e2c1eb45e9f55f77fdf68bcee6f
Kevin Duggan <kevinduggan95@gmail.com>
2024-05-16 14:38:49 -0500

    changes for CFT

---
+++ src/GoogleMapsHelp.js
@@ -0,0 +1,48 @@
+import React, { useState,useEffect } from "react";
+import jd from './Images/jd.jpeg'
+import horacio from './Images/horacio.jpeg'
+import clou from './Images/clou.jpeg'
+import team from './Images/341260897_241927845028189_3175328261831266190_n.jpeg'
+import OGs from './Images/aboutheader.jpeg'
+import cBelal from './Images/cBelal.jpeg'
+// import jd2 from'./Images/jd2.jpeg'
+import bjj from './Images/jiujitsuT.jpeg'
+
+
+export default function CFTImageReel() {
+
+const [CFTImages, setCFTImages] = useState(clou);
+useEffect(() => {
+    const images = [cBelal,OGs,'https://mmajunkie.usatoday.com/wp-content/uploads/sites/91/2015/11/horacio-gutierrez-ufc-fight-night-78.jpg',
+                        team,'https://cst.brightspotcdn.com/dims4/default/78b48a8/2147483647/strip/true/resize/840x555!/quality/90/?url=https%3A%2F%2Fcdn.vox-cdn.com%2Fthumbor%2FJdRT2W7BMkzX2V1RNYpAwtD-rpw%3D%2F0x0%3A6000x4000%2F6000x4000%2Ffilters%3Afocal%283000x2000%3A3001x2001%29%2Fcdn.vox-cdn.com%2Fuploads%2Fchorus_asset%2Ffile%2F16273912%2Floutaylor_042719_03_1_of_1.jpg',
+                        'https://cst.brightspotcdn.com/dims4/default/1d9b0d7/2147483647/strip/true/resize/840x555!/quality/90/?url=https%3A%2F%2Fcdn.vox-cdn.com%2Fthumbor%2FlG8quCpb59CmN89loxzlqdMseeM%3D%2F0x0%3A5530x3798%2F5530x3798%2Ffilters%3Afocal%282765x1899%3A2766x1900%29%2Fcdn.vox-cdn.com%2Fuploads%2Fchorus_asset%2Ffile%2F16273864%2Floutaylor_042719_10_1_of_1.jpg',
+                        // jd2,
+                        'https://images.tapology.com/gym_people/logos/4963/thumb/4963-roberto-ramirez.jpg?1656437404',
+                         jd,'https://dmxg5wxfqgb4u.cloudfront.net/styles/card_advance_small_280x356/s3/2023-05/GettyImages-1488060577.jpg?itok=nqtoylPJ',bjj, clou,];
+    let index = 0;
+
+    const interval = setInterval(() => {
+      index = (index + 1) % images.length;
+      setCFTImages(images[index]);
+    }, 1000);
+
+    return () => clearInterval(interval); // Clear interval on component unmount
+  }, []);
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+
+ 
+
+      return (
+        <div style={{borderRadius:'50%', marginLeft: '10px', marginRight: '5%'}}>
+        <center>
+                <div style={{borderRadius:'50%',width:'200px',height:'200px',backgroundImage: `url(${CFTImages})`,backgroundSize:'100% 100%'}} >
+                
+                 {/* <GoogleMapsHelp/> */}
+                </div>
+                </center>
+        </div>
+      )
+    }
--- src/Home.js
+++ src/Home.js
@@ -1,44 +1,94 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 // import InstagramEmbed from 'react-instagram-embed';
 import './App.css';
+import ZOOM1 from './Images/ZOOM1.png'
+import ZOOM2 from './Images/ZOOM2.png'
+import ZOOM3 from './Images/ZOOM3.png'
+import GoogleMapsHelp from './GoogleMapsHelp.js'
+
 
 
 
 function Home() {
 
     const [chiTown, setChiTown] = useState(true);
-
+    
+    const [gooogleMapsI, setGooogleMapsI] = useState(ZOOM1);
+    useEffect(() => {
+        const images = [ZOOM1, ZOOM3, ZOOM2,];
+        let index = 0;
+    
+        const interval = setInterval(() => {
+          index = (index + 1) % images.length;
+          setGooogleMapsI(images[index]);
+        }, 3000);
+    
+        return () => clearInterval(interval); // Clear interval on component unmount
+      }, []);
     const toggleGym = () => {
         setChiTown(!chiTown);
     };
+
+    
   
   
     return (
       <div  >
         
         {chiTown ?
-        <div className = "home">
-        <h2>Welcome Home</h2>
+        <div className = "home" style={{marginBottom:'30px'}}>
+       <div className="flex" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: '30px' }}>
         
+        <GoogleMapsHelp /><h2 style={{background: 'rgba(255, 255, 255, 0.5)'}}>Home to World Champions</h2>
+      </div>
             <h1>Chicago Fight Team</h1>
 
-            <a style={{ textAlign:'center' }}href="https://www.google.com/maps/place/8696+47th+St,+Lyons,+IL+60534/@41.8060748,-87.8398832,17z/data=!4m10!1m2!2m1!1schicago+fight+team++8696+w+47th+st+lyons+illinois+!3m6!1s0x880e361341753f5b:0x777f5a692510f0a6!8m2!3d41.8059629!4d-87.8363641!15sCjFjaGljYWdvIGZpZ2h0IHRlYW0gIDg2OTYgdyA0N3RoIHN0IGx5b25zIGlsbGlub2lzkgEQY29tcG91bmRfc2VjdGlvbuABAA!16s%2Fg%2F11bw4nfy1c?entry=ttu"
+
+
+<div style={{marginBottom:'0px',marginTop:'50px'}}>
+            <a style={{ textAlign:'center' , marginTop:'30px' }}href="https://www.google.com/maps/place/8696+47th+St,+Lyons,+IL+60534/@41.8060748,-87.8398832,17z/data=!4m10!1m2!2m1!1schicago+fight+team++8696+w+47th+st+lyons+illinois+!3m6!1s0x880e361341753f5b:0x777f5a692510f0a6!8m2!3d41.8059629!4d-87.8363641!15sCjFjaGljYWdvIGZpZ2h0IHRlYW0gIDg2OTYgdyA0N3RoIHN0IGx5b25zIGlsbGlub2lzkgEQY29tcG91bmRfc2VjdGlvbuABAA!16s%2Fg%2F11bw4nfy1c?entry=ttu"
      target="_blank" >
-                <span style={{ background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '5px', fontWeight: 'bold', textShadow: '0 0 15px black', }}>
+                <span style={{ background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '5px', fontWeight: 'bold', textShadow: '0 0 15px black'}}>
                 8696 W 47th St Lyons, Illinois
                 </span> </a>
-                <a href="tel:+17082794734" style={{ textAlign:'center' }}>
-                <span style={{background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '5px', fontWeight: 'bold', textShadow: '0 0 15px black' }}>
-                1(773) 885-5700
-                </span>
-            </a>
-
+                <br></br>
+        
+               
+</div>
             {/* <p>6528 S Lavernge Ave Chicago, Illinois</p>
             <p></p> */}
 
+<center>
+                <div style={{border:'black solid 3px',borderRadius:'10%',width:'55%',height:'300px',backgroundImage: `url(${gooogleMapsI})`,backgroundSize:'100% 100%'}} >
+                
+                 {/* <GoogleMapsHelp/> */}
+                </div>
+                </center>
 
+                <a
+          href="https://www.google.com/maps/place/8696+47th+St,+Lyons,+IL+60534/@41.8060748,-87.8398832,17z/data=!4m10!1m2!2m1!1schicago+fight+team++8696+w+47th+st+lyons+illinois+!3m6!1s0x880e361341753f5b:0x777f5a692510f0a6!8m2!3d41.8059629!4d-87.8363641!15sCjFjaGljYWdvIGZpZ2h0IHRlYW0gIDg2OTYgdyA0N3RoIHN0IGx5b25zIGlsbGlub2lzkgEQY29tcG91bmRfc2VjdGlvbuABAA!16s%2Fg%2F11bw4nfy1c?entry=ttu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button
+            style={{
+              padding: '10px 20px',
+              background: '#007bff',
+              color: 'white',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+              textAlign: 'center', marginBottom:'30px' 
+            }}
+          >
+            Get Directions
+          </button>
+        </a>
             
-            <button onClick={toggleGym}>Take me to Guadalajara</button>
+
+
+
+            {/* <button onClick={toggleGym}>Take me to Guadalajara</button> */}
             <center>
                 <iframe
                     class="igh"
@@ -52,6 +102,60 @@ function Home() {
 
                 ></iframe>
                 </center>
+
+
+
+                {/* <a href="tel:+17082794734" style={{ textAlign:'center' }}>
+                <span style={{background: 'rgba(0, 0, 0, 0.5)', color: 'white', padding: '5px', fontWeight: 'bold', textShadow: '0 0 15px black' }}>
+                1(773) 885-5700
+                </span>
+                     </a> */}
+                {/* //////////////////////////////////////// */}
+               <div style={{background: 'rgba(255, 255, 255, 0.5)', width:'85%', margin:'7.5%', borderRadius:'20%'}} className=" shadow sm:rounded-lg">
+      <div className="px-4 py-5 sm:p-6">
+        <div className="sm:flex sm:items-start sm:justify-between">
+          <div>
+            <h3 className="text-base font-semibold leading-6 text-gray-900">Get Started Today</h3>
+            <div className="mt-2 max-w-xl text-sm text-gray-500">
+              <p>
+              Schedule your training session at Chicago Fight Team today! 
+              </p>
+            </div>
+          </div>
+          <div className="mt-5 sm:ml-6 sm:mt-0 sm:flex sm:flex-shrink-0 sm:items-center">
+            <button 
+              style={{
+                padding: '10px 20px',
+                background: '#007bff',
+                color: 'white',
+                border: 'none',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                textAlign: 'center', marginBottom:'30px' 
+              }}
+              type="button"
+              className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            ><a  style={{
+                color: 'white', 
+              }}href="tel:+17738855700" className="hover:underline">Call Us</a>
+              
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+                
+               
+                
+                
+                
+                
+                
+                
+                
                 </div> : <div className="home2"><div className="fish">
         <h2>Welcome Home</h2>
         
